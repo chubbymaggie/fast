eb4341596c9b3a2833e5e018c43321b48be50fb5
Yijun Yu <yijun.yu@gmail.com>
2017-07-09 15:25:07 +0100

    merged the functionalities of process and slice-diff into the fast command as -l and -L options respectively

--- src/gen/ver.h
+++
@@ -1,3 +0,0 @@
-#define __FAST_VERSION__ "v0.0.3"
-#define __FAST_HASH__ "ac095ab2c9ac3da8c4ec8e629bec14b316f47e0b"
-#define __FAST_WORK__ "447b98baf63b0d65c9d80f00c708ff079d3378c3d27210d3a48507c420ef926c"
