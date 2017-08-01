dd1d67d18bd80f914a977c4c6b598f1c0613f05b
Yijun Yu <y.yu@open.ac.uk>
2017-08-01 13:23:44 +0000

    update fast's debian package

--- src/gen/ver.h
+++
@@ -1,3 +0,0 @@
-#define __FAST_VERSION__ "v0.0.4"
-#define __FAST_HASH__ "12558c69e1ea6c72db16e5a6da401a43c3b95ab5"
-#define __FAST_WORK__ "6b0e6a14fedebd4995957a1e5be197230c6bcb17b017a6e39f5c95e04bf31649"
