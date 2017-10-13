20d229fad0faa2de2680414753e418ec324d75c4
Yijun Yu <yijun.yu@gmail.com>
2017-10-13 13:52:46 +0100

    update version number

---
+++ src/gen/ver.h
@@ -0,0 +1,3 @@
+#define __FAST_VERSION__ "v0.0.6"
+#define __FAST_HASH__ "198181473f9a1a9cc3fac0e7a8c6c6a1cadeb88f"
+#define __FAST_WORK__ "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
